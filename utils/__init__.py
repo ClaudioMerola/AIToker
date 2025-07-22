eeed2facf100eb374a22acbb7296a6761d5bd888
Claudio Merola <Merola@outlook.com>
2025-07-22 18:32:35 -0300

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from .ai import func_openai, func_azureopenai
+from .audio import func_core_audiocreator, func_azure_text_to_speech, func_tiktok_texttospeach
+from .browser import func_take_sitescreenshot
+from .video import func_core_videocreator, func_core_subtitlescreator
