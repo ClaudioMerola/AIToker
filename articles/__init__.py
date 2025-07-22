ca57af227f6fb75ed736f4797e8a2991ae73a681
Claudio Merola <Merola@outlook.com>
2025-07-22 18:30:35 -0300

    Create __init__.py

---
+++ articles/__init__.py
@@ -0,0 +1,5 @@
+from .editor import func_editor
+from .nytimes import func_nytimes_newsroom
+from .cnn import func_cnn_newsroom
+from .bbc import func_bbc_newsroom
+from .post import func_washingtonpost_newsroom
